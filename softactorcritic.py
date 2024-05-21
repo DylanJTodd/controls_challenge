0c12e8595523302095c1af37e02bf5636f099ee2
DylanJTodd <dylan.todd04@gmail.com>
2024-05-21 11:36:26 -0400

    added buffer and critic classes

--- softactorcritic.py
+++ softactorcritic.py
@@ -2,9 +2,11 @@
 #batch Metrics python tinyphysics.py --model_path ./models/tinyphysics.onnx --data_path ./data --num_segs 100 --controller simple
 #Generate comparison report python eval.py --model_path ./models/tinyphysics.onnx --data_path ./data --num_segs 100 --test_controller simple --baseline_controller open
 
+#Time:                                      t
 #Car Velocity:                              v_ego
 #Forward Acceleration:                      a_ego
 #lateral acceleration due to road roll:     road_lataccel
+
 #Current car lateral acceleration:          current_lataccel
 #Steer input:                               steer_action
 
@@ -32,6 +34,13 @@ from tinyphysics import TinyPhysicsModel, TinyPhysicsSimulator, CONTROL_START_ID
     #Evaluates actions taken by actor. Estimates the expected return (Q-value) of taking a specific action given a specific state.
     #Inputs: Current state, and current action
     #Outputs: Individual estimate of expected return (Q-value) associated with the state-action pair.
+
+        #Ensure to add:
+        #Random variable weight initialization 
+        #Maybe bootstrapping (different subsets of data), 
+        #Hyperparameter variability
+        #Boosting (THIS IS A MUST, SOUNDS SUPER COOL), boosting is the process of training 
+
 
 #Replay Buffer
     #Stores experiences during an agents interactions with environment. Breaks temporal correlations by shuffling and taking random buffer samples.
@@ -66,11 +75,70 @@ class ActorNetwork(nn.Module):
         return steering, acceleration
 
 
-class CriticNetwork(nn.Module):
-    print("filler function")
-    #Ensure to add:
-        #Random weight initialization 
-        #Maybe bootstrapping (different subsets of data), 
-        #Hyperparameter variability
-        #Boosting (THIS IS A MUST, SOUNDS SUPER COOL)
+
+class CriticNetwork(nn.Module): 
+    def __init__(self):
+        super(CriticNetwork, self).__init__()
+        self.layer1 = nn.Linear(5, 512) #Excludes time as input
+        self.layer2 = nn.Linear(512, 256)
+        self.layer3 = nn.Linear(256, 64)
+        self.layer4 = nn.Linear(64, 1)
+    
+    def forward(self, state, action):
+        q_value = nn.functional.relu(self.layer1(torch.cat([state, action],1)))
+        q_value = nn.functional.relu(self.layer2(q_value))
+        q_value = nn.functional.relu(self.layer3(q_value))
+        q_value = self.layer4(q_value)
+
+
+
+class ReplayBuffer(object):
+    def __init__(self, state_dimensions, action_dimensions):
+        self.max_size = int(200000)
+        self.count = 0
+        self.size = 0
+        self.state = numpy.zeros((self.max_size, state_dimensions))
+        self.action = numpy.zeros((self.max_size, action_dimensions))
+        self.reward = numpy.zeros((self.max_size, 1))
+        self.next_state = numpy.zeros((self.max_size, state_dimensions))
+        self.terminal_state = numpy.zeros((self.max_size, 1))
+
+
+    #Stores transition (current state, action taken, reward received, next observed state, terminal state indicator)
+    #Count points to the next available slot in the buffer, with index wrap around max_size
+    #Size reflects current number of transitions stored
+    def store (self, state, action, reward, next_state, terminal_state):
+        self.state[self.count] = state
+        self.action[self.count] = action
+        self.reward[self.count] = reward
+        self.next_state[self.count] = next_state
+        self.terminal_state[self.count] = terminal_state
+        self.count = (self.count + 1) % self.max_size #When 'count' reaches max_size, it will be reset to 0
+        self.size = min(self.size + 1, self.max_size) #Record the number of transitions
+
+
+    #Randomly selects a batch of transitions
+    #Each transition contains above mentioned features
+    #transitions converted to pytorch tensors
+    def sample (self, batch_size):
+        index = numpy.random.choice(self.size, size = batch_size)
+        batch_state = torch.tensor(self.state[index], dtype = torch.float32)
+        batch_action = torch.tensor(self.action[index], dtype = torch.float32)
+        batch_reward = torch.tensor(self.reward[index], dtype = torch.float32)
+        batch_next_state = torch.tensor(self.next_state[index], dtype = torch.float32)
+        batch_terminal_state = torch.tensor(self.terminal_state[index], dtype = torch.float32)
+
+        return batch_state, batch_action, batch_reward, batch_next_state, batch_terminal_state
+    
+
+class SAC(object):
+     print("filler")
+    
+
+    
+
+
+    
+
+
 
