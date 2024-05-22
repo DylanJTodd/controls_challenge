4937267121911a216f763cf279c7061fbce20f80
DylanJTodd <dylan.todd04@gmail.com>
2024-05-22 16:39:04 -0400

    added sacController to controllers

---
+++ controllers/sacController.py
@@ -0,0 +1,15 @@
+import torch
+import numpy
+
+class SACController:
+    def __init__(self, actor_network, critic_network, log_std):
+        self.actor_network = actor_network
+        self.critic_network = critic_network
+        self.log_std = log_std
+
+    def update(self, target_lataccel, current_lataccel, state):
+        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
+        steering, acceleration = self.actor_network(state_tensor)
+
+        action = torch.cat((steering, acceleration), dim=1).squeeze(0).numpy()
+        return action
