5efc0eb5cc89d32c41b6998d2f4fd2cca126cf07
DylanJTodd <dylan.todd04@gmail.com>
2024-05-22 21:30:23 -0400

    Fixed formatting to proper controller

--- controllers/sacController.py
+++ controllers/sacController.py
@@ -1,7 +1,7 @@
 import torch
 import numpy
-
-class sacController:
+from . import BaseController
+class Controller(BaseController):
     def __init__(self, actor_network, critic_network, log_std):
         self.actor_network = actor_network
         self.critic_network = critic_network
@@ -10,6 +10,5 @@ class sacController:
     def update(self, target_lataccel, current_lataccel, state):
         state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
         steering, acceleration = self.actor_network(state_tensor)
-
         action = torch.cat((steering, acceleration), dim=1).squeeze(0).numpy()
         return action
